d17adb895d1ccf089888973c4e9342262bd17852
“Natalia <natalia.rudiger@gmail.com>
2022-03-09 16:20:13 -0300

    iteration 2 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import contacts from './contacts.json';
 
-const newContacts = contacts.slice(0,5)
+const newContacts = contacts.slice(0,6)
+const trophy = '🏆'
+
+
+
 
 function App() {
   return (
@@ -13,14 +17,20 @@ function App() {
             <td><h2>Picture</h2></td>
             <td><h2>Name</h2></td>
             <td><h2>Popularity</h2></td>
+            <td><h2>Won<br/>Oscar</h2></td>
+            <td><h2>Won<br/>Emmy</h2></td>
           </tr>
           
       { newContacts.map(props => {
+        const wonOscar = props.wonOscar ? trophy : null
+        const wonEmmy = props.wonEmmy ? trophy : null
         return (
           <tr>
             <td><img src = {props.pictureUrl} alt = {props.name}/></td>
             <td><p>{props.name}</p></td>
             <td><p>{Math.round(props.popularity * 100) / 100}</p></td>
+            <td><span>{wonOscar}</span></td>
+            <td><span>{wonEmmy}</span></td>
           </tr>
         )
       })}
