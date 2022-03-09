7bf6898be6d2df0a8ac498125bc2efbe37759d2b
“Natalia <natalia.rudiger@gmail.com>
2022-03-09 14:06:12 -0300

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import contacts from './contacts.json';
+
+const newContacts = contacts.slice(0,5)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <div className = 'contacts-container'>
+        <table cellpadding="0" cellspacing="0">
+          <tr>
+            <td><h2>Picture</h2></td>
+            <td><h2>Name</h2></td>
+            <td><h2>Popularity</h2></td>
+          </tr>
+          
+      { newContacts.map(props => {
+        return (
+          <tr>
+            <td><img src = {props.pictureUrl} alt = {props.name}/></td>
+            <td><p>{props.name}</p></td>
+            <td><p>{Math.round(props.popularity * 100) / 100}</p></td>
+          </tr>
+        )
+      })}
+        </table>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
